b74afecc5ae3fbd21b9d69cb654bd89db5e57099
Zoty <34866157+ZotyDev@users.noreply.github.com>
2021-01-18 14:11:25 -0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/%{cfg.buildcgf}/%{prj.name}")
+	objdir ("bin-int/%{cfg.buildcgf}/%{prj.name}")
 
 	files
 	{
